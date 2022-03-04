2436db921ef08e3a743dbcc647af0d5b704943a3
Anaroco <roblescortes@msn.com>
2022-03-04 19:49:19 +0100

    new repositry

---
+++ src/components/FinalMark.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="container">
+    <h1 class="text-center mt-5">Grade Report</h1>
+    <div class="d-flex mt-5">
+      <div class="row">
+        <div class="mt-4">
+          <input
+            id="name"
+            type="text"
+            v-model="name"
+            placeholder="Name Student"
+            class="w-100 form-control"
+            required
+          />
+        </div>
+        <div class="mt-4">
+          <input
+            type="text"
+            v-model="subject"
+            placeholder="Subject"
+            class="w-100 form-control"
+            required
+          />
+        </div>
+        <div class="mt-4">
+          <input
+            type="number"
+            v-model="mark"
+            placeholder="Score"
+            class="w-100 form-control"
+            required
+          />
+        </div>
+        <div class="mt-4">
+          <button @click="submitMark" class="btn btn-secondary rounded-0">
+            Add Grade
+          </button>
+        </div>
+      </div>
+    </div>
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th>Name Student</th>
+          <th>Subject</th>
+          <th>Final Mark</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(student, index) in students" :key="index">
+          <td>{{ student.name }}</td>
+          <td>{{ student.subject }}</td>
+          <td>{{ student.mark }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FinalMark",
+  data: () => {
+    return {
+      name: "",
+      subject: "",
+      mark: "",
+      students: [
+        {
+          name: "Ana",
+          subject: "Java",
+          mark: 8,
+        },
+      ],
+    };
+  },
+  methods: {
+    submitMark() {
+      // console.log(this.name);
+
+      this.students.push({
+        name: this.name,
+        subject: this.subject,
+        mark: this.mark,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  color: #b64c8d;
+  margin-top: 60px;
+}
+
+.mt-5 {
+  margin-top: 3rem !important;
+  justify-content: center;
+}
+
+thead {
+  background-color: rgb(218, 115, 171);
+}
+</style>
